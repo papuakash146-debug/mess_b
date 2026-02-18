2cdc9ed879f4ac71987d4de6f2b82e474ca38d48
Akash Dev <akashddev200@gmail.com>
2026-02-18 12:14:40 +0530

    final commit

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import itemRoutes from "./routes/itemRoutes.js";
 import usageRoutes from "./routes/usageRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/items", itemRoutes);
 app.use("/api/usage", usageRoutes);
